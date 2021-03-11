16861e675c004cd5ccc14f45088424cbfddb1b5c
rdbende <rdbende@gmail.com>
2021-03-11 11:36:29 +0100

    Update LinkLabel.py

--- LinkLabel.py
+++ LinkLabel.py
@@ -1,127 +1,118 @@
-"""Simple clickable link widget for tkinter"""
+"""
+Author: rdbende
+License: GNU GPLv3
+Copyright (c): 2021 rdbende
+"""
 
-# Based on RedFantom's LinkLabel: (https://github.com/TkinterEP/ttkwidgets/blob/master/ttkwidgets/linklabel.py)
+# Based on RedFantom's LinkLabel: (https://github.com/RedFantom/ttkwidgets/blob/master/ttkwidgets/linklabel.py)
+
 import tkinter as tk
 from tkinter import ttk
 import webbrowser
 
 class LinkLabel(ttk.Label):
-    """
-        If you want a widget that opens a website you should use LinkLabel
+    """Clickable label that opens a link"""
+    def __init__(self, master=None, **kwargs):
+        """
+        Create a clickable label
+
+        Options:
         
-        Standard label options:
-            anchor, background, class, compound
-            cursor, font, foreground, image
-            justify, style, takefocus, text
-            textvariable, underline, padding
-            relief, width, wraplength
+            hovercolor (hex-color): text color when hovering over the widget
+            normalcolor (hex-color): text color at the init of the widget
+            url (str): the link to be open
+            visited (bool): set the label to visited, (False by default)
+            visitedcolor (hex-color): text color when link is clicked
+            kwargs: options to be passed on to the ttk.Label initializer
+            
+        Generates:
 
-        Widget-specific options:
-            hovercolor (hex-color): Color when you hover the label
-            url (str): The link that you want to open
-            visited (bool): Sets the link to be visited, (default is False)
-            visitedcolor (hex-color): Color after the label was clicked
-
-        Virtual Event:
-            <<LinkOpened>>
-
-        Widget method:
-            clear : Clears the visited, and hovered statement
-
+            virtual event: <<LinkOpened>>
+            
         Variable:
-            is_visited : True or False
-
-        Usage:
-            link = LinkLabel(master, text='LinkLabel', url='https://github.com/rdbende/Tkinter-extension-widgets')
-            link.pack()
-    """
-    
-    
-    def __init__(self, master, **kwargs):
         
+            is_visited: True or False
+            
+        Method:
+            
+            reset: reset the visited, and hovered statement
+        """
         self._url = kwargs.pop("url", "https://")
-        self._normalcolor = kwargs.pop("foreground", "#0007ff")
+        self._normalcolor = kwargs.pop("normalcolor", "#0007ff")
         self._hovercolor = kwargs.pop("hovercolor", "#00009f")
-        self._visitedcolor = kwargs.pop("visitedcolor", "#6600aa")
-        self._visited = kwargs.pop("visited", False)
-        if root.tk.call('tk', 'windowingsystem') == 'win32':
-            self._cursor = kwargs.pop("cursor", "hand2")
-        elif root.tk.call('tk', 'windowingsystem') == 'aqua':
+        self._visitedcolor = kwargs.pop("visitedcolor", "#660099")
+        self.is_visited = kwargs.pop("visited", False)
+        self._master = master or tk._default_root
+        if self._master.tk.call('tk', 'windowingsystem') == 'aqua':
             self._cursor = kwargs.pop("cursor", "pointinghand")
         else:
-            self._cursor = kwargs.pop("cursor", "hand1")
-        self.is_visited = self._visited
-        ttk.Label.__init__(self, master, **kwargs, cursor=self._cursor)
+            self._cursor = kwargs.pop("cursor", "hand2")
+        ttk.Label.__init__(self, master, cursor=self._cursor, foreground=self._normalcolor, **kwargs)
         self.bind("<Button-1>", self._open)
         self.bind("<Enter>", self._enter)
         self.bind("<Leave>", self._leave)
         self._leave()
 
-        
+    def __getitem__(self, key):
+        return self.cget(key)
+
+    def __setitem__(self, key, value):
+        self.configure(**{key: value})
+
     def _enter(self, *args):
-        if self._visited:
+        if self.is_visited:
             self.config(foreground=self._visitedcolor)
         else:
             self.config(foreground=self._hovercolor)
 
-            
     def _leave(self, *args):
-        if self._visited:
+        if self.is_visited:
             self.config(foreground=self._visitedcolor)
         else:
             self.config(foreground=self._normalcolor)
 
-            
     def _open(self, *args):
-        """Opens the given url in the default webbrowser"""
-        self._visited = True
-        self.is_visited = self._visited
-        self.event_generate('<<LinkOpened>>')
+        """Open the given url in the default webbrowser"""
         webbrowser.open(self._url)
+        self.is_visited = True
+        self._leave()
+        self.event_generate("<<LinkOpened>>")
+
+    def reset(self):
+        """Reset the visited, and hovered statement"""
+        self.is_visited = False
         self._leave()
 
-        
-    def clear(self):
-        """Clears the visited, and hovered statement"""
-        self._visited = False
-        self.is_visited = self._visited
-        self._leave()
-        
+    def configure(self, **kwargs):
+        """Configure resources of the widget"""
+        self._url = kwargs.pop("url", self._url)
+        self._normalcolor = kwargs.pop("normalcolor", self._normalcolor)
+        self._hovercolor = kwargs.pop("hovercolor", self._hovercolor)
+        self._visitedcolor = kwargs.pop("visitedcolor", self._visitedcolor)
+        self.is_visited = kwargs.pop("visited", self.is_visited)
+        self._cursor = kwargs.pop("cursor", self._cursor)
+        ttk.Label.configure(self, cursor=self._cursor, **kwargs)
+
+    config = configure
         
     def cget(self, key):
         """Return the resource value for a KEY given as string"""
         if key == "hovercolor":
             return self._hovercolor
+        elif key == "normalcolor":
+            return self._normalcolor
         elif key == "url":
             return self._url
-        elif key == "visited":
-            return self._visited
         elif key == "visitedcolor":
             return self._visitedcolor
+        elif key == "visited":
+            return self.is_visited
         else:
             return ttk.Label.cget(self, key)    
-    
     
     def keys(self):
         """Return a list of all resource names of this widget"""
         keys = ttk.Label.keys(self)
-        keys.extend(["hovercolor", "url", "visited", "visitedcolor"])
-        keys = sorted(keys)
+        keys.extend(["hovercolor", "normalcolor", "url", "visited", "visitedcolor", ])
+        keys.sort()
         return keys
-
-# Test
-
-if __name__ == '__main__':
-    
-    root = tk.Tk()
-    root.title('LinkLabel')
-    root.geometry('220x70')
-    
-    def callback(event):
-        print('Link clicked!')
-        
-    label = LinkLabel(root, url='https://github.com/rdbende/Tkinter-extension-widgets', text='Open repo', font=('TkDefaultFont', 10, 'underline'))
-    label.pack(pady=20)
-    label.bind('<<LinkOpened>>', callback)
-    
-    root.mainloop()
