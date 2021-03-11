98e88c32081fd2ae7a63377f881ccb373943947e
rdbende <rdbende@gmail.com>
2021-03-11 17:24:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,17 @@
-from .Theme import *
-from .LinkLabel import *
-from .ToolTip import *
-from .ToggledFrame import *
-from .PopupMenu import *
-from .MenuBar import *
-from .tkImage import *
+"""
+Tkinter extension widgets
+
+Author: rdbende
+License: GNU GPLv3
+Copyright (c): 2021 rdbende
+
+Source: https://github.com/rdbende/Tkinter-extension-widgets
+"""
+
+from .ToolTip import ToolTip
+from .NumberEntry import NumberEntry
+from .tkImage import Image
+from .PopupMenu import PopupMenu
+from .MenuBar import MenuBar
+from .LinkLabel import LinkLabel # Based on RedFantom's LinkLabel
+from .ToggledFrame import ToggledFrame # Based on RedFantom's ToggledFrame
